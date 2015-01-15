dcf5605d6bf31e06bab39cf486a6a230df479876
Arman Bilge <armanbilge@gmail.com>
2015-01-15 23:36:36 +1300

    Continue testing

--- src/hmc/BirthDeathSimulator.java
+++ src/hmc/BirthDeathSimulator.java
@@ -6,12 +6,17 @@ import beast.evolution.tree.Tree;
 import beast.evolution.util.Taxa;
 import beast.inference.model.Parameter;
 import beast.math.MathUtils;
+import beast.util.FileHelpers;
 import beast.xml.AbstractXMLObjectParser;
 import beast.xml.ElementRule;
 import beast.xml.XMLObject;
 import beast.xml.XMLParseException;
 import beast.xml.XMLSyntaxRule;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
@@ -53,7 +58,13 @@ public class BirthDeathSimulator extends AbstractXMLObjectParser<SimpleTree> {
             nodes.add(p);
         }
         final SimpleTree tree = new SimpleTree(removeRandomElement(nodes));
-        System.out.println(Tree.Utils.newick(tree));
+        if (xo.hasId()) try (final PrintWriter out = new PrintWriter(new FileOutputStream(FileHelpers.getFile(xo.getId() + ".tre")))) {
+            out.write(Tree.Utils.newick(tree));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return tree;
     }
 
